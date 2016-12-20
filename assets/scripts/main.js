92746bcf654bea91b260b5c6e7893063c6457223
Giel Cobben <giel@mollie.com>
2016-12-20 17:43:39 +0100

    Working on Dock

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,7 @@
-$(document).ready(() => {
-    console.log('ready.');
-})
+const dock = document.getElementsByClassName('dock')[0];
+const caption = document.getElementsByClassName('icon-caption')[0];
+
+setTimeout(() => {
+    dock.classList.remove('hide');
+    caption.classList.add('animate');
+}, 1000)
