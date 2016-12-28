9b13caebd8dbb0bab8616fa3100c6cf2ece23679
Giel Cobben <giel@mollie.com>
2016-12-28 10:10:11 +0100

    Fixed sprites + animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,24 @@
+const app = document.getElementsByClassName('img')[0];
+const info = document.getElementsByClassName('info')[0];
 const dock = document.getElementsByClassName('dock')[0];
 const caption = document.getElementsByClassName('icon-caption')[0];
 
 setTimeout(() => {
     dock.classList.remove('hide');
-    caption.classList.add('animate');
+
+    setTimeout(() => {
+
+        caption.classList.add('animate');
+
+        setTimeout(() => {
+
+            info.classList.remove('middle');
+            app.classList.remove('hide');
+            caption.classList.remove('animate');
+            // dock.classList.add('hide');
+
+        }, 1200)
+
+    }, 100)
+
 }, 1000)
