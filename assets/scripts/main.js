abc6ddc5a8f664a795ef0a4449423871f72ebb69
Giel Cobben <giel@mollie.com>
2016-12-30 15:56:15 +0100

    Working on design details

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,24 +6,24 @@ const caption = document.getElementsByClassName('icon-caption')[0];
 // element
 imagesLoaded( document.querySelector('body'), (instance) => {
 
-    setTimeout(() => {
-        dock.classList.remove('hide');
-
-        setTimeout(() => {
-
-            caption.classList.add('animate');
-
-            setTimeout(() => {
-
-                info.classList.remove('middle');
-                app.classList.remove('hide');
-                caption.classList.remove('animate');
-                // dock.classList.add('hide');
-
-            }, 1200)
-
-        }, 100)
-
-    }, 1000)
+    // setTimeout(() => {
+    //     dock.classList.remove('hide');
+    //
+    //     setTimeout(() => {
+    //
+    //         caption.classList.add('animate');
+    //
+    //         setTimeout(() => {
+    //
+    //             info.classList.remove('middle');
+    //             app.classList.remove('hide');
+    //             caption.classList.remove('animate');
+    //             // dock.classList.add('hide');
+    //
+    //         }, 1200)
+    //
+    //     }, 100)
+    //
+    // }, 1000)
 
 })
