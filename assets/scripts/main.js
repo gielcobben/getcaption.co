d2b217ac609abb12eca9fa81a31a24cc58bb703c
Giel <g.cobben@gmail.com>
2017-01-07 15:55:42 +0100

    Add Ticker

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,20 +4,15 @@ $(document).ready(() => {
     const featureItems = $('.features li');
     const featureIcons = $('.content .icon');
 
-    // $(featureIcons).hover(() => {
-    //     $(downloadButton).addClass('ticker');
-    // }, () => {
-    //     $(downloadButton).removeClass('ticker');
-    // });
-
     for (let i = 0; i < featureIcons.length; i++) {
+
         const featureIcon = featureIcons[i];
 
         $(featureIcon).hover(() => {
             $(featureItems[i]).addClass('show');
         }, () => {
             $(featureItems[i]).removeClass('show');
-        })
+        });
 
     }
 
