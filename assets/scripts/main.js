9cf8047fdff1f89e1f9d9669f741f179b58000ff
Giel Cobben <giel@mollie.com>
2017-01-11 15:09:07 +0100

    Clean up + Responsive

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
 
+    // Elements
     const downloadButton = $('.download');
     const featureItems = $('.features li');
     const featureIcons = $('.content .icon');
 
+    // Ticker
     for (let i = 0; i < featureIcons.length; i++) {
 
         const featureIcon = featureIcons[i];
@@ -14,6 +16,12 @@ $(document).ready(() => {
             $(featureItems[i]).removeClass('show');
         });
 
+    }
+
+    // Remove download button on mobile
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        const html = '<span class="button">Available on MacOS...</span>'
+        downloadButton.html(html)
     }
 
 });
