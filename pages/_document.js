d9dcd2d86cfe4cf50140c5d28b925a50b554b92c
Vernon de Goede <vernon@ecurring.nl>
2017-11-17 11:28:44 +0100

    Set document title using React Helmet

---
+++ pages/_document.js
@@ -0,0 +1,46 @@
+import Document, { Head, Main, NextScript } from "next/document";
+import Helmet from "react-helmet";
+
+export default class extends Document {
+  static async getInitialProps(...args) {
+    const documentProps = await super.getInitialProps(...args);
+    return { ...documentProps, helmet: Helmet.renderStatic() };
+  }
+
+  get helmetHtmlAttrComponents() {
+    return this.props.helmet.htmlAttributes.toComponent();
+  }
+
+  get helmetBodyAttrComponents() {
+    return this.props.helmet.bodyAttributes.toComponent();
+  }
+
+  get helmetHeadComponents() {
+    return Object.keys(this.props.helmet)
+      .filter(el => el !== "htmlAttributes" && el !== "bodyAttributes")
+      .map(el => this.props.helmet[el].toComponent());
+  }
+
+  get helmetJsx() {
+    return (
+      <Helmet htmlAttributes={{ lang: "en" }}>
+        <title>Caption – Start watching</title>
+      </Helmet>
+    );
+  }
+
+  render() {
+    return (
+      <html {...this.helmetHtmlAttrComponents}>
+        <Head>
+          {this.helmetJsx}
+          {this.helmetHeadComponents}
+        </Head>
+        <body {...this.helmetBodyAttrComponents}>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
